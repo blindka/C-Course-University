542253a917f298a9cecc9a8da9b644731c7fd4ea
Eyal Haimov <eyalhaimoon@gmail.com>
2021-11-10 08:35:33 +0200

    Updated
    Removing unused variable - Landing

--- Q2.C
+++ Q2.C
@@ -4,28 +4,28 @@
 int main() 
 {
     /* H - hours, M - minutes, S - Seconds, Flight - Flight time, Landing - when the rocket will land, leftover - if it is more than 24 hours - days*/
-    int H, M, S, Flight, Landing, leftover;
+    int H, M, S, Flight, leftover;
     printf("Enter START TIME (H,M,S): \n");
     scanf("%d %d %d",&H, &M, &S);
     /* checks if the value of hours, minutes, seconds is valid, otherwise - exit and stop the program*/
     if(H < 24 || H > 0 || M < 60 || M > 0 || S < 60 || S > 0)
     {
-        if((H >= 24 || H < 0) && (M >= 60 || M < 0) && (S >= 60 || S < 0))
+        if((H >= 24 || H < 0)&&(M >= 60 || M < 0)&&(S >= 60 || S < 0))
         {
           printf("You Enter Invalid Inputs: Please Enter Hours between 0-23, Minutes and Seconds between 0-59.");
           return 0;
         }
-        if((M >= 60 || M < 0) && (S >= 60 || S < 0))
+        if((M >= 60 || M < 0)&&(S >= 60 || S < 0))
         {
            printf("You Enter Invalid Inputs: Minutes and Seconds Input, Please Enter between 0-59.\n");
            return 0;
         }
-        if((H >= 24 || H < 0) && (M >= 60 || M < 0))
+        if((H >= 24 || H < 0)&&(M >= 60 || M < 0))
         {
           printf("You Enter Invalid Inputs: Please Enter Hours between 0-24, Minutes  between 0-59.\n");
           return 0;
         }
-        if((H >= 24 || H < 0) && (S >= 60 || S < 0))
+        if((H >= 24 || H < 0)&&(S >= 60 || S < 0))
         {
           printf("You Enter Invalid Inputs: Please Enter Hours between 0-23, Seconds  between 0-59.\n");
           return 0;
@@ -48,17 +48,17 @@ int main()
     }
     printf("Enter FLIGHT TIME in seconds: \n");
     scanf("%d", &Flight);
-    /*flight time is only in seconds (less than minute)*/
+    /* flight time is only in seconds (less than minute) */
         if(Flight<=60)
         {
             S = S + Flight;
-            if(S>60) /* after adding value - if the seconds greater than 60 - adding to minutes*/
+            if(S>60) /* after adding value - if the seconds greater than 60 - adding to minutes */
             {
                M = M + (S/60);
                S = S - 60;
             }
         }
-        /*flight time is in minutes (less than hours)*/
+        /* flight time is in minutes (less than hours) */
         if(Flight>60 && Flight<3600)
         {
             M = M + (Flight/60);
@@ -74,11 +74,11 @@ int main()
               S = S - 60;
             }
         }
-        /*flight time is in hours*/
+        /* flight time is in hours */
         if(Flight>3600)
         {
-            H = H + (Flight/3600); /*adding the value to hours*/
-            M = M + ((Flight/60) % 60); /*adding the value to minutes*/
+            H = H + (Flight/3600); /* adding the value to hours */
+            M = M + ((Flight/60) % 60); /* adding the value to minutes */
             
         }
     if(H > 0 && M == 0 && S == 0)
@@ -101,7 +101,7 @@ int main()
         printf("The LANDING will take place at %d hour, %d second",H,S);
         if(!(H == 1) && S==1)
         printf("The LANDING will take place at %d hours, %d second",H,S);
-        if(H>24) /*if we get more than 24 hours - than outputting +days*/
+        if(H>24) /* if we get more than 24 hours - than outputting +days */
         {
         leftover = H / 24;
         H = H % 24;
